c8e61a7312388fe0a09ee3d5af219cd93445dc3f
p0pl3 <m.poplavskiy04@gmail.com>
2022-04-03 17:23:47 +0300

    FIX: Edit test.py

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import CategoryFeed, Feed, Tag, ContactAddress
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.home_page_url = reverse('HomePage')
+
+    def test_HomePage_get(self):
+        response = self.client.get(self.home_page_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/index.html')
+
+    def test_FeedDetail_get(self):
+        response = self.client.get(reverse('FeedDetail', kwargs=[]))
+        core1 = Feed.objects.create(
+        )
+
+
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, 'core/blog-post-img.html')
