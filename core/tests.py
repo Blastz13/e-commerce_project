5fd9d88881ec4e2f28fbcc396fef37dfb428e6b3
p0pl3 <m.poplavskiy04@gmail.com>
2022-04-07 17:41:47 +0300

    FIX: Code review unit-tests

--- core/tests.py
+++ core/tests.py
@@ -17,8 +17,8 @@ class TestViewHomePage(TestCase):
 
 class TestViewFeedDetail(TestCase):
     def setUp(self):
-        self.category = CategoryFeed.objects.create(title="HOT_NEWS", slug="hot_news")
-        self.feed = Feed.objects.create(title="Hot news", body="Lorem Ipsum", slug="hot_news",
+        self.category = CategoryFeed.objects.create(title="HOT NEWS", slug="hot_news")
+        self.feed = Feed.objects.create(title="New book", body="Lorem Ipsum", slug="new_book",
                                         category=self.category, is_slider=True, is_publish=True, is_blog=True)
 
     def test_request_get(self):
@@ -31,27 +31,27 @@ class TestViewFeedDetail(TestCase):
     def test_create_comment_post(self):
         """Checking the creation of a news comment"""
         response = self.client.post(self.feed.get_absolute_url(), data={
-            "email": "test@mail.ru",
-            "name": "Test_User",
+            "email": "ivan_pupkin@mail.ru",
+            "name": "Ivan",
             "text": "Good Luck!"
         })
-        comment = Comment.objects.get(email="test@mail.ru", name="Test_User")
+        comment = Comment.objects.get(email="ivan_pupkin@mail.ru", name="Ivan")
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(str(comment), "Test_User - Hot news")
+        self.assertEqual(str(comment), "Ivan - New book")
 
     def test_create_comment_with_parent_post(self):
         """Checking the creation of news comment with parent"""
-        comment_parent = Comment.objects.create(email="test1@mail.ru", name="Test_User1", feed=self.feed)
+        comment_parent = Comment.objects.create(email="ivan_pupkin@mail.ru", name="Ivan", feed=self.feed)
         response = self.client.post(self.feed.get_absolute_url(), data={
-            "email": "test2@mail.ru",
-            "name": "Test_User2",
-            "text": "Good Luck2!",
+            "email": "lena_golovach@mail.ru",
+            "name": "Elena",
+            "text": "Very bad shop!",
             "parent": comment_parent.id
         })
 
         self.assertEqual(response.status_code, 302)
-        self.assertIsNotNone(Comment.objects.get(email="test2@mail.ru", name="Test_User2", text="Good Luck2!",
+        self.assertIsNotNone(Comment.objects.get(email="lena_golovach@mail.ru", name="Elena", text="Very bad shop!",
                                                  parent=comment_parent.id))
 
     def tearDown(self):
@@ -73,22 +73,22 @@ class TestViewFeedList(TestCase):
 
 class TestViewFeedListCategory(TestCase):
     def setUp(self):
-        self.category_feed = CategoryFeed.objects.create(title="Test_Category", slug="test_category")
+        self.category = CategoryFeed.objects.create(title="HOT NEWS", slug="hot_news")
 
     def test_request_get(self):
         """Page retrieval check FeedListCategory"""
-        response = self.client.get(self.category_feed.get_absolute_url())
+        response = self.client.get(self.category.get_absolute_url())
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'core/blog.html')
 
     def tearDown(self):
-        self.category_feed.delete()
+        self.category.delete()
 
 
 class TestViewFeedListTag(TestCase):
     def setUp(self):
-        self.tag = Tag.objects.create(title="Test_Tag", slug="test_tag")
+        self.tag = Tag.objects.create(title="Science Fiction", slug="science_fiction")
 
     def test_request_get(self):
         """Page retrieval check FeedListTag"""
@@ -112,28 +112,28 @@ class TestViewLeaveMessage(TestCase):
     def test_send_contact_message_post(self):
         """"Checking contact message creation"""
         response = self.client.post(reverse('LeaveMessage'), data={
-            "email": "test@mail.ru",
-            "name": "Test_User",
-            "subject": "Test_Subject",
+            "email": "ivan_pupkin@mail.ru",
+            "name": "Ivan",
+            "subject": "THANKS",
             "message": "Good Luck!"
         })
-        contact = ContactForm.objects.get(email="test@mail.ru", name="Test_User")
+        contact = ContactForm.objects.get(email="ivan_pupkin@mail.ru", name="Ivan")
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(str(contact), "test@mail.ru - Test_Subject")
+        self.assertEqual(str(contact), "ivan_pupkin@mail.ru - THANKS")
 
 
 class TestModelFeed(TestCase):
     def setUp(self):
-        self.category = CategoryFeed.objects.create(title="HOT_NEWS", slug="hot_news")
-        self.feed = Feed.objects.create(title="Hot news", body="Lorem Ipsum", slug="hot_news",
+        self.category = CategoryFeed.objects.create(title="HOT NEWS", slug="hot_news")
+        self.feed = Feed.objects.create(title="New book", body="Lorem Ipsum", slug="new_book",
                                         category=self.category, is_slider=True, is_publish=True, is_blog=True)
-        self.comment1 = Comment.objects.create(email="test1@mail.ru", name="Test_User1", feed=self.feed)
-        self.comment2 = Comment.objects.create(email="test2@mail.ru", name="Test_User2", feed=self.feed)
+        self.comment1 = Comment.objects.create(email="ivan_pupkin@mail.ru", name="Ivan", feed=self.feed)
+        self.comment2 = Comment.objects.create(email="lena_golovach@mail.ru", name="Elena", feed=self.feed)
 
     def test_get_abolute_url(self):
         """Test geting url of Feed"""
-        self.assertEqual(self.feed.get_absolute_url(), '/blog/hot_news/hot_news/')
+        self.assertEqual(self.feed.get_absolute_url(), '/blog/hot_news/new_book/')
 
     def test_get_comment(self):
         """Test getting comments in Feed"""
@@ -141,7 +141,7 @@ class TestModelFeed(TestCase):
 
     def test_str(self):
         """Test cast Feed to string"""
-        self.assertEqual(str(self.feed), "Hot news")
+        self.assertEqual(str(self.feed), "New book")
 
     def tearDown(self):
         self.category.delete()
